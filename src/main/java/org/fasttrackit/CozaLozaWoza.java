f93ba28597764fa4dcc20ad97280c498bce08bc0
Keseru Szabolcs <keserusz94@gmail.com>
2019-09-23 19:28:17 +0300

    Sa scrie toate spatiile intr-un rand

--- src/main/java/org/fasttrackit/CozaLozaWoza.java
+++ src/main/java/org/fasttrackit/CozaLozaWoza.java
@@ -7,25 +7,26 @@ public class CozaLozaWoza {
 
         for (int i = start; i <= finish; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print("CozaLoza ");
+                System.out.print("CozaLoza");
             }
             else if ( i % 3 == 0){
-                System.out.print("Coza ");
+                System.out.print("Coza");
             }
             else if (i % 5 == 0) {
-                System.out.print(("Loza "));
+                System.out.print(("Loza"));
             }
             else if (i % 7 == 0) {
-                System.out.print("Woza ");
+                System.out.print("Woza");
             }
             else {
-                System.out.print(i + " ");
+                System.out.print(i);
             }
 
             if (i % 11 == 0){
                 System.out.println(" ");
+            } else {
+            System.out.print(" ");
             }
-
         }
     }
 }
